e1de4ea99ad783bfd54f1073b328e712f49f2cd7
Ryan Wilson <ryan.wilson@empiriolabs.io>
2021-03-08 23:32:34 -0500

    initial commit

---
+++ src/ProblemSet13.java
@@ -0,0 +1,30 @@
+public class ProblemSet13 {
+
+    public boolean groupSum(int start, int[] numbers, int target) {
+
+    }
+
+    public boolean groupSum6(int start, int[] numbers, int target) {
+
+    }
+
+    public boolean groupNoAdj(int start, int[] numbers, int target) {
+
+    }
+
+    public boolean groupSum5(int start, int[] numbers, int target) {
+
+    }
+
+    public boolean groupSumClump(int start, int[] numbers, int target) {
+
+    }
+
+    public boolean splitArray(int[] numbers) {
+
+    }
+
+    public boolean splitOdd(int[] numbers) {
+        
+    }
+}
